195b9b794ad94237bcf546b1eaa6daa2ecd26bc1
Zackk2504 <zackkingvn@gmail.com>
2024-10-02 14:25:31 +0700

    123
    123

---
+++ backend/src/main/java/com/yellowcat/backend/model/Dichvu.java
@@ -0,0 +1,77 @@
+package com.yellowcat.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "dichvu")
+public class Dichvu {
+    @Id
+    @JsonProperty
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dichvu_id_gen")
+    @SequenceGenerator(name = "dichvu_id_gen", sequenceName = "dichvu_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @JsonProperty
+    @Column(name = "tendichvu", nullable = false, length = 100)
+    private String tendichvu;
+
+    @JsonProperty
+    @Column(name = "mota", nullable = false, length = Integer.MAX_VALUE)
+    private String mota;
+
+    @JsonProperty
+    @Column(name = "anh", length = Integer.MAX_VALUE)
+    private String anh;
+
+    @JsonProperty
+    @Column(name = "giatien", nullable = false)
+    private Integer giatien;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTendichvu() {
+        return tendichvu;
+    }
+
+    public void setTendichvu(String tendichvu) {
+        this.tendichvu = tendichvu;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public String getAnh() {
+        return anh;
+    }
+
+    public void setAnh(String anh) {
+        this.anh = anh;
+    }
+
+    public Integer getGiatien() {
+        return giatien;
+    }
+
+    public void setGiatien(Integer giatien) {
+        this.giatien = giatien;
+    }
+}
--- backend/src/main/java/com/yellowcat/backend/repository/DichvuRepository.java
+++ backend/src/main/java/com/yellowcat/backend/repository/DichvuRepository.java
@@ -1,10 +1,13 @@
 package com.yellowcat.backend.repository;
 
-import com.yellowcat.backend.model.DichVu;
+
+import com.yellowcat.backend.model.Dichvu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface DichvuRepository extends JpaRepository<DichVu, Long> {
 
+public interface DichvuRepository extends JpaRepository<Dichvu, Long> {
 
+    List<Dichvu> findByTendichvuContainingIgnoreCase(String tendichvu);
 }
