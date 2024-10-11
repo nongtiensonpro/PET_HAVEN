ae34ff5c20b03490d165662e2ea92407a556b31f
Zackk2504 <zackkingvn@gmail.com>
2024-10-12 01:04:40 +0700

    check-role-keycloak
    show role , username and id user

---
+++ backend/src/main/java/com/yellowcat/backend/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.yellowcat.backend.controller;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.GrantedAuthority;
+
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user")
+    public String getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String idUser = authentication.getName();
+
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        Map<String, Object> resourceAccess = (Map<String, Object>) jwt.getClaim("resource_access");
+        List<String> petHavenRoles = null;
+        if (resourceAccess != null) {
+            Map<String, Object> petHavenAccess = (Map<String, Object>) resourceAccess.get("PetHaven");
+            if (petHavenAccess != null) {
+                petHavenRoles = (List<String>) petHavenAccess.get("roles");
+            }
+        }
+        String username = jwt.getClaimAsString("preferred_username");
+        if (username == null) {
+            username = jwt.getClaimAsString("email");
+        }
+
+        return "Roles: " + petHavenRoles + " | idUser: " + idUser + " | username: " + username;
+    }
+
+
+}
