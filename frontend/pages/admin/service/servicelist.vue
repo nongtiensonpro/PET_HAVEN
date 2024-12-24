242c79d25eca5dfc259aa4a9d59029a459ad5b99
Nông Tiến Sơn <68463083+nongtiensonpro@users.noreply.github.com>
2024-12-24 13:48:06 +0700

    Cập  nhật Dịch Vụ

--- frontend/pages/admin/service/servicelist.vue
+++ frontend/pages/admin/service/servicelist.vue
@@ -1,362 +1,154 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import {useServiceStore} from '~/stores/DichVuStores';
-import {useNotificationStore} from '~/stores/useNotificationStore';
-import {useUserStore} from '~/stores/user';
-import {useToast} from 'vue-toastification';
-import InputField from '~/components/InputField.vue';
-import {useForm} from 'vee-validate';
-import * as yup from 'yup';
-import UpdateDichVu from "~/components/UpdateDichVu.vue";
+import { onMounted, ref, computed } from 'vue';
+import { useServiceStore } from '~/stores/DichVuStores';
+import { useNotificationStore } from '~/stores/useNotificationStore';
+import { useUserStore } from '~/stores/user';
+import { useToast } from 'vue-toastification';
 
 definePageMeta({
   middleware: ['auth']
 })
 
-// Add these new refs for pagination
 const currentPage = ref(1);
 const itemsPerPage = 5;
-
-// Compute paginated services
-const paginatedServices = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  return serviceStore.services.slice(start, end);
-});
-
-// Compute total pages
-const totalPages = computed(() => {
-  return Math.ceil(serviceStore.services.length / itemsPerPage);
-});
-
-// Methods for pagination
-const nextPage = () => {
-  if (currentPage.value < totalPages.value) {
-    currentPage.value++;
-  }
-};
-
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--;
-  }
-};
+const name = ref('');
 
 const toast = useToast();
 const serviceStore = useServiceStore();
 const notificationStore = useNotificationStore();
 const user = useUserStore();
 
-const service = ref({
-  tendichvu: '',
-  giatien: '',
-  mota: '',
-  trangthai: false,
-  anh: null as File | null,
+const paginatedServices = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return serviceStore.services.slice(start, end);
 });
 
-const anh = ref(null);
+const totalPages = computed(() => Math.ceil(serviceStore.services.length / itemsPerPage));
 
-const selectedFile = ref<File | null>(null);
-
-const handleFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files.length > 0) {
-    selectedFile.value = target.files[0];
-    anh.value = URL.createObjectURL(selectedFile.value);
-  }
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) currentPage.value++;
 };
 
-
-const tendichvuRules = 'required|min:1';
-const giatienRules = 'required|numeric|min:0';
-const motaRules = 'required';
-
-const schema = yup.object().shape({
-  tendichvu: yup.string().required('Vui lòng nhập tên dịch vụ'),
-  giatien: yup.number()
-      .typeError('Giá tiền phải là một số')
-      .required('Vui lòng nhập giá tiền')
-      .positive('Giá tiền phải lớn hơn 0'),
-  mota: yup.string().required('Vui lòng nhập mô tả')
-});
-
-const {handleSubmit, resetForm} = useForm({
-  validationSchema: schema,
-  initialValues: {
-    tendichvu: '',
-    giatien: '',
-    mota: '',
-    trangthai: false,
-    anh: null,
-  }
-});
-const submitForm = handleSubmit(async (formValues) => {
-  await createService(formValues);
-});
-
-onMounted(() => {
-  serviceStore.fetchServices();
-});
-
-const cleanService = () => {
-  service.value.tendichvu = '';
-  service.value.giatien = '';
-  service.value.mota = '';
-  service.value.trangthai = false;
-  anh.value = '';
-  resetForm()
-  toast.success("Các ô input đã được làm sạch.");
+const prevPage = () => {
+  if (currentPage.value > 1) currentPage.value--;
 };
 
-const createService = async (formValues) => {
-  try {
-    const result = await serviceStore.addDichVu(formValues);
-    if (result.success) {
-      notificationStore.addNotification('Dịch vụ đã được tạo thành công', user.userInfo.name);
-      toast.success('Dịch vụ đã được tạo thành công!');
-      await serviceStore.fetchServices();
-      cleanService();
-      resetForm();
-    } else {
-      notificationStore.addNotification(`Có lỗi xảy ra khi tạo dịch vụ: ${result.message}`, user.userInfo.name);
-      toast.error(`Có lỗi xảy ra khi tạo dịch vụ: ${result.message}`);
-    }
-  } catch (error) {
-    notificationStore.addNotification('Có lỗi xảy ra khi tạo dịch vụ', user.userInfo.name);
-    toast.error('Có lỗi xảy ra khi tạo dịch vụ.');
-  }
-};
+onMounted(serviceStore.fetchServices);
 
-
-const name = ref('');
-
-const findServiceByName = async (name: string) => {
-  if (!name || name.trim() === '') {
+const findServiceByName = async () => {
+  if (!name.value.trim()) {
     toast.error('Tên dịch vụ không được để trống.');
-    setTimeout(() => {
-      serviceStore.fetchServices();
-    }, 2000);
+    setTimeout(serviceStore.fetchServices, 2000);
     return;
   }
   try {
-    const result = await serviceStore.getDichVuByName(name);
-    if (!result.status) {
-      toast.error(result.message);
-    }
+    const result = await serviceStore.getDichVuByName(name.value);
+    if (!result.status) toast.error(result.message);
   } catch (error) {
     toast.error('Đã xảy ra lỗi khi tìm kiếm. Vui lòng thử lại sau.');
-    setTimeout(() => {
-      serviceStore.fetchServices();
-    }, 2000);
-  }
-};
-const deleteService = async (serviceId: String) => {
-  try {
-    const result = await serviceStore.deleteDichVu(serviceId);
-
-    if (result.success) {
-      notificationStore.addNotification('Dịch vụ đã được xóa thành công', user.userInfo.name);
-      toast.success('Dịch vụ đã được xóa thành công!');
-      await serviceStore.fetchServices();
-    } else {
-      notificationStore.addNotification(`Có lỗi xảy ra khi xóa dịch vụ: ${result.message}`, user.userInfo.name);
-      toast.error(`Có lỗi xảy ra khi xóa dịch vụ: ${result.message}`);
-    }
-  } catch (error) {
-    notificationStore.addNotification('Có lỗi xảy ra khi xóa dịch vụ', user.userInfo.name);
-    toast.error('Có lỗi xảy ra khi xóa dịch vụ.');
+    setTimeout(serviceStore.fetchServices, 2000);
   }
 };
 
-const updateTTService = async (serviceId: String) => {
+const handleServiceAction = async (action, serviceId, successMessage, errorMessage) => {
   try {
-    await serviceStore.updateTTDV(serviceId);
-    notificationStore.addNotification("Dịch vụ đã được cập nhật thành công!", user.userInfo.name);
-    toast.success('Dịch vụ đã được cập nhật thành công!');
+    await action(serviceId);
+    notificationStore.addNotification(successMessage, user.userInfo.name);
+    toast.success(successMessage);
     await serviceStore.fetchServices();
   } catch (error) {
-    notificationStore.addNotification("Dịch vụ đã được cập nhật thất bại!", user.userInfo.name);
-    toast.error('Dịch vụ đã được cập nhật thất bại!');
+    notificationStore.addNotification(errorMessage, user.userInfo.name);
+    toast.error(errorMessage);
   }
 };
 
-const updateTTHienService = async (serviceId: String) => {
-  try {
-    await serviceStore.updateTTHien(serviceId);
-    notificationStore.addNotification("Dịch vụ đã được cập nhật thành công!", user.userInfo.name);
-    toast.success('Dịch vụ đã được cập nhật thành công!');
-    await serviceStore.fetchServices();
-  } catch (error) {
-    notificationStore.addNotification("Dịch vụ đã được cập nhật thất bại!", user.userInfo.name);
-    toast.error('Dịch vụ đã được cập nhật thất bại!');
-  }
-};
+const deleteService = (serviceId) => handleServiceAction(
+  serviceStore.deleteDichVu,
+  serviceId,
+  'Dịch vụ đã được xóa thành công!',
+  'Có lỗi xảy ra khi xóa dịch vụ.'
+);
+
+const updateTTService = (serviceId) => handleServiceAction(
+  serviceStore.updateTTDV,
+  serviceId,
+  'Dịch vụ đã được cập nhật thành công!',
+  'Dịch vụ đã được cập nhật thất bại!'
+);
+
+const updateTTHienService = (serviceId) => handleServiceAction(
+  serviceStore.updateTTHien,
+  serviceId,
+  'Dịch vụ đã được cập nhật thành công!',
+  'Dịch vụ đã được cập nhật thất bại!'
+);
+
+const chiTietDV = (serviceId) => navigateTo(`/admin/service/${serviceId}`);
 </script>
 
 <template>
   <div class="container card bg-light p-4" style="border-radius: 25px">
     <div class="container">
-      <div class="text fs-1">
-        Tổng quan dịch vụ
-      </div>
+      <h1 class="fs-1">Tổng quan dịch vụ</h1>
       <div class="row p-4">
         <div class="col-10">
           <div class="form-group">
             <div class="row">
               <div class="col-8">
-                <input type="text" class="custom-button" name="" id="" style="min-width: 100% !important;" aria-describedby="helpId" placeholder="Tìm kiếm ?"
-                       v-model="name">
+                <input v-model="name" type="text" class="custom-button" style="min-width: 100% !important;" placeholder="Tìm kiếm ?">
               </div>
               <div class="col-2">
-                <button type="button" class="custom-button" @click="findServiceByName(name)">
-                  Tìm kiếm
-                </button>
+                <button @click="findServiceByName" type="button" class="custom-button">Tìm kiếm</button>
               </div>
               <div class="col-2">
-                <button type="button" class="custom-button" @click="serviceStore.fetchServices()">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                       class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                <button @click="serviceStore.fetchServices" type="button" class="custom-button">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
-                    <path
-                        d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
                   </svg>
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-2">
-          <button type="button" class="custom-button" data-bs-toggle="modal" data-bs-target="#modal">
-            Thêm dịch vụ
-          </button>
-          <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" >
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="modalLabel">Thêm dịch vụ:
-                    {{ service.tendichvu }}</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <form v-on:submit.prevent="submitForm">
-                    <div class="row">
-                      <div class="col-6">
-                        <div v-if="anh===null">
-                          <img :src="service.anh" class="card-img-top p-1" alt="Không có ảnh rùi">
-                        </div>
-                        <div v-else>
-                          <img :src="anh" class="card-img-top p-1" alt="Không có ảnh rùi">
-                        </div>
-                        <input type="file" id="fileInput" accept="image/png, image/jpeg, image/gif"
-                               @change="handleFileChange"/>
-                      </div>
-                      <div class="col-6">
-                        <InputField
-                            type="text"
-                            name="tendichvu"
-                            class="form-control"
-                            label="Tên dịch vụ"
-                            v-model="service.tendichvu"
-                            :rules="tendichvuRules"
-                        />
-                        <InputField
-                            type="number"
-                            name="giatien"
-                            class="form-control"
-                            label="Giá tiền"
-                            v-model="service.giatien"
-                            :rules="giatienRules"
-                        />
-                        <div class="form-check form-control form-check-inline">
-                          <label class="form-check-label">
-                            <input class="form-check-input" type="radio" name="trangthai" v-model="service.trangthai"
-                                   :value="true"> Hoạt động
-                            <br>
-                            <input class="form-check-input" type="radio" name="trangthai" v-model="service.trangthai"
-                                   :value="false"> Không hoạt động
-                          </label>
-                        </div>
-                      </div>
-                      <div class="col-12">
-                        <InputField
-                            type="text"
-                            name="mota"
-                            class="form-control"
-                            label="Mô tả"
-                            v-model="service.mota"
-                            :rules="motaRules"
-                        />
-                      </div>
-                    </div>
-                    <div class="p-12 p-1">
-                      <div class="row">
-                        <div class="col">
-                          <button type="button" class="custom-button" data-bs-dismiss="modal">Đóng</button>
-                        </div>
-                        <div class="col">
-                          <button type="submit" class="custom-button">Thêm dịch vụ</button>
-                        </div>
-                        <div class="col">
-                          <button type="button" class="custom-button" @click="cleanService">Clean</button>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
     <table class="table">
       <thead>
-      <tr>
-        <th class="text-center" scope="col">STT</th>
-        <th class="text-center" scope="col">Tên dịch vụ</th>
-        <th class="text-center" scope="col">Trạng thái</th>
-        <th class="text-center">Hành động</th>
-      </tr>
+        <tr>
+          <th class="text-center" scope="col">STT</th>
+          <th class="text-center" scope="col">Tên dịch vụ</th>
+          <th class="text-center" scope="col">Trạng thái</th>
+          <th class="text-center">Hành động</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="(service, index) in serviceStore.services" :key="service.id">
-        <th class="text-center" scope="row">{{ index + 1 }}</th>
-        <td class="text-center">{{ service.tendichvu }}</td>
-        <td class="text-center">{{ service.trangthai == true ? 'Hoạt động' : 'Không hoạt động' }}</td>
-        <td>
-          <div  class="row">
-            <div class="col">
-              <button type="button" class="btn btn-sm btn-outline-danger" @click="deleteService(service.id)" data-bs-dismiss="modal">Xóa
-              </button>
+        <tr v-for="(service, index) in paginatedServices" :key="service.id">
+          <th class="text-center" scope="row">{{ (currentPage - 1) * itemsPerPage + index + 1 }}</th>
+          <td class="text-center">{{ service.tendichvu }}</td>
+          <td class="text-center">{{ service.trangthai ? 'Hoạt động' : 'Không hoạt động' }}</td>
+          <td>
+            <div class="row">
+              <div class="col">
+                <button @click="deleteService(service.id)" type="button" class="btn btn-sm btn-outline-danger">Xóa</button>
+              </div>
+              <div class="col">
+                <button @click="updateTTService(service.id)" type="button" class="btn btn-sm btn-outline-warning">
+                  {{ service.trangthai ? 'Ẩn dịch vụ' : 'Hiện dịch vụ' }}
+                </button>
+              </div>
+              <div class="col">
+                <button @click="updateTTHienService(service.id)" type="button" class="btn btn-sm btn-outline-secondary">
+                  {{ service.hien ? 'Ẩn khỏi màn hình chính' : 'Hiện ở màn hình chính' }}
+                </button>
+              </div>
+              <div class="col">
+                <button @click="chiTietDV(service.id)" type="button" class="btn btn-sm btn-outline-success m-1">Chi tiết</button>
+              </div>
             </div>
-            <div class="col">
-              <button type="button" class="btn btn-sm btn-outline-warning" @click="updateTTService(service.id)" data-bs-dismiss="modal"  >
-                <span v-if="service.trangthai">
-                  Ẩn dịch vụ
-                </span>
-                <span v-else>
-                  Hiện dịch vụ
-                </span>
-              </button>
-            </div>
-            <div class="col">
-              <button type="button" class="btn btn-sm btn-outline-secondary" @click="updateTTHienService(service.id)" data-bs-dismiss="modal" >
-                <span v-if="service.hien">
-                  Ẩn khỏi màn hình chính
-                </span>
-                <span v-else>
-                  Hiện ở màn hình chính
-                </span>
-              </button>
-            </div>
-            <div class="col">
-              <update-dich-vu :service="service"/>
-            </div>
-          </div>
-        </td>
-      </tr>
+          </td>
+        </tr>
       </tbody>
     </table>
     <div class="pagination d-flex justify-content-center align-items-center mt-3">
@@ -371,11 +163,7 @@ const updateTTHienService = async (serviceId: String) => {
           <button @click="nextPage" :disabled="currentPage === totalPages" class="custom-button">Sau</button>
         </div>
       </div>
-
-
-
     </div>
-
   </div>
 </template>
 
--- frontend/stores/DichVuStores.ts
+++ frontend/stores/DichVuStores.ts
@@ -32,9 +32,9 @@ export const useServiceStore = defineStore('serviceStore', {
             formData.append('trangThai', service.trangthai);
 
             const fileInput = document.querySelector('#fileInput') as HTMLInputElement;
-                if (fileInput && fileInput.files && fileInput.files.length > 0) {
-                    formData.append('file', fileInput.files[0]);
-                }
+            if (fileInput && fileInput.files && fileInput.files.length > 0) {
+                formData.append('file', fileInput.files[0]);
+            }
             try {
                 const response = await fetch(API_ENDPOINTS.API_ENDPOINTS.dichVu.addDichVu, {
                     method: 'POST',
@@ -43,13 +43,13 @@ export const useServiceStore = defineStore('serviceStore', {
                     },
                     body: formData
                 });
-        
+
                 if (!response.ok) {
-                    return { success: false, message: response.status };
+                    return {success: false, message: response.status};
                 }
-                return { success: true, message: 'Thêm dịch vụ thành công' };
+                return {success: true, message: 'Thêm dịch vụ thành công'};
             } catch (error) {
-                return { success: false, message: 'Lỗi thêm dịch vụ' };
+                return {success: false, message: 'Lỗi thêm dịch vụ'};
             }
         },
         async updateDichVu(service: DichVu) {
@@ -83,9 +83,9 @@ export const useServiceStore = defineStore('serviceStore', {
                 console.log("Dữ liệu nhận được từ server:", data); // In ra dữ liệu nhận được
                 this.services = data.content;
                 this.pageable = data.page;
-                return { success: true, message: 'Lưu thành công' };
+                return {success: true, message: 'Lưu thành công'};
             } catch (error) {
-                return { success: false, message: `Đã có lỗi xảy ra: ` + error.message };
+                return {success: false, message: `Đã có lỗi xảy ra: ` + error.message};
             }
         }
         ,
@@ -118,7 +118,7 @@ export const useServiceStore = defineStore('serviceStore', {
             const token = localStorage.getItem('access_token');
 
             if (!token) {
-                return { status: false, message: 'Vui lòng đăng nhập lại để sử dụng dịch vụ' };
+                return {status: false, message: 'Vui lòng đăng nhập lại để sử dụng dịch vụ'};
             }
             try {
                 const response = await fetch(updateDichVuUrl, {
@@ -134,16 +134,16 @@ export const useServiceStore = defineStore('serviceStore', {
                     this.services = data.content;
                     this.pageable = data.page;
                     console.log("Dữ liệu trả về: ", data.content);
-                    return { status: true, message: 'Tìm kiếm hoạt động ổn' };
+                    return {status: true, message: 'Tìm kiếm hoạt động ổn'};
                 } else if (response.status === 404) {
-                    return { status: false, message: 'Không tìm thấy gì' };
+                    return {status: false, message: 'Không tìm thấy gì'};
                 } else if (response.status === 500) {
-                    return { status: false, message: 'Lỗi máy chủ vui lòng thử lại' };
+                    return {status: false, message: 'Lỗi máy chủ vui lòng thử lại'};
                 } else {
-                    return { status: false, message: `Lỗi không xác định mã ${response.status}` };
+                    return {status: false, message: `Lỗi không xác định mã ${response.status}`};
                 }
             } catch (error) {
-                return { status: false, message: 'Đã có lỗi máy chủ xảy ra vui lòng thử lại' };
+                return {status: false, message: 'Đã có lỗi máy chủ xảy ra vui lòng thử lại'};
             }
         }
         ,
@@ -201,7 +201,65 @@ export const useServiceStore = defineStore('serviceStore', {
                 throw error;
             }
         },
+        async updateService(dichVu: DichVu) {
+            console.log(" Dữ liệu sẽ gửi đi "+ JSON.stringify(dichVu));
+            const updateDichVuUrl = API_ENDPOINTS.API_ENDPOINTS.dichVu.updateDichVu + dichVu.id;
+            const token = localStorage.getItem('access_token');
+            const formDataUpdate = new FormData();
 
+            formDataUpdate.append('tenDichVu', dichVu.tendichvu);
+            formDataUpdate.append('moTa', dichVu.mota);
+            formDataUpdate.append('giaTien', dichVu.giatien);
+            formDataUpdate.append('trangThai', dichVu.trangthai.toString());
+            formDataUpdate.append('hien', dichVu.hien.toString());
+
+            // Xử lý các tùy chọn dịch vụ
+            dichVu.tuyChonDichVus.forEach((tuyChon, index) => {
+                formDataUpdate.append(`tuyChonDichVus[${index}].tenTuyChon`, tuyChon.tentuychon);
+                formDataUpdate.append(`tuyChonDichVus[${index}].moTa`, tuyChon.mota);
+                formDataUpdate.append(`tuyChonDichVus[${index}].trangThai`, tuyChon.trangthai.toString());
+
+                tuyChon.tuyChonCanNangs.forEach((canNang, canNangIndex) => {
+                    formDataUpdate.append(`tuyChonDichVus[${index}].tuyChonCanNangs[${canNangIndex}].canNangMin`, canNang.cannangmin.toString());
+                    formDataUpdate.append(`tuyChonDichVus[${index}].tuyChonCanNangs[${canNangIndex}].canNangMax`, canNang.cannangmax?.toString() || '');
+                    formDataUpdate.append(`tuyChonDichVus[${index}].tuyChonCanNangs[${canNangIndex}].giaTien`, canNang.giatien.toString());
+                    formDataUpdate.append(`tuyChonDichVus[${index}].tuyChonCanNangs[${canNangIndex}].trangThai`, canNang.trangthai.toString());
+                });
+            });
+
+            // Xử lý file ảnh nếu có
+            if (dichVu.file instanceof File) {
+                formDataUpdate.append('file', dichVu.file);
+            }
+
+            try {
+                const response = await fetch(updateDichVuUrl, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                    body: formDataUpdate,
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Đã có lỗi xảy ra: ${response.status}`);
+                }
+
+                const data = await response.json();
+                console.log("Dữ liệu nhận được từ server:", data);
+
+                // Cập nhật state
+                const index = this.services.findIndex(s => s.id === dichVu.id);
+                if (index !== -1) {
+                    this.services[index] = data;
+                }
+
+                return {success: true, message: 'Cập nhật dịch vụ thành công'};
+            } catch (error) {
+                console.error('Lỗi khi cập nhật dịch vụ:', error);
+                return {success: false, message: `Đã có lỗi xảy ra: ${error.message}`};
+            }
+        },
         // Hàm làm sạch payload
         cleanPayload(payload: any) {
             return JSON.parse(JSON.stringify(payload, (key, value) => {
